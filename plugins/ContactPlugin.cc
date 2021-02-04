4586b5b36468a98cb486669d229aed79ae7ec93a
Vincenzo Suriani <vincsur@gmail.com>
2021-02-04 14:44:07 +0100

    contact sensor

--- plugins/ContactPlugin.cc
+++ plugins/ContactPlugin.cc
@@ -11,15 +11,12 @@
 using namespace gazebo;
 GZ_REGISTER_SENSOR_PLUGIN(ContactPlugin)
 
-/////////////////////////////////////////////////
 ContactPlugin::ContactPlugin() : SensorPlugin() {
 }
 
-/////////////////////////////////////////////////
 ContactPlugin::~ContactPlugin() {
 }
 
-/////////////////////////////////////////////////
 void ContactPlugin::Load(sensors::SensorPtr _sensor, sdf::ElementPtr /*_sdf*/) {
 
   if (!ros::isInitialized()) {
@@ -56,61 +53,38 @@ void ContactPlugin::Load(sensors::SensorPtr _sensor, sdf::ElementPtr /*_sdf*/) {
   this->parentSensor->SetActive(true);
 }
 
-/////////////////////////////////////////////////
 void ContactPlugin::OnUpdate() {
+
+  msg.data = 0.0f;
 
   // Get all the contacts.
   msgs::Contacts contacts;
   contacts = this->parentSensor->Contacts();
-  for (unsigned int i = 0; i < contacts.contact_size(); ++i) {
-
-    std::cout << "Collision between[" << contacts.contact(i).collision1()
-              << "] and [" << contacts.contact(i).collision2() << "]\n";
-
-    
+  for (unsigned int i = 0; i < contacts.contact_size(); ++i) {  
 
     if ((contacts.contact(i).collision1().find("pushcart")!=std::string::npos || 
         contacts.contact(i).collision2().find("pushcart")!=std::string::npos) && 
         ((contacts.contact(i).collision1().find("reemc")!=std::string::npos || 
         contacts.contact(i).collision2().find("reemc")!=std::string::npos) ) ) {
         
-        std::cout << "########################## collision between cart and any robot link \n";
+//        std::cout << " collision between cart and any robot link \n";
         math::Vector3 actualForce;
         
         for (unsigned int j = 0; j < contacts.contact(i).position_size(); ++j) {
-            std::cout << j << "  Position:"
-                << contacts.contact(i).position(j).x() << " "
-                << contacts.contact(i).position(j).y() << " "
-                << contacts.contact(i).position(j).z() << "\n";
-            
+                    
             actualForce.x =
                 contacts.contact(i).wrench(j).body_1_wrench().force().x();
             actualForce.y =
                 contacts.contact(i).wrench(j).body_1_wrench().force().y();
             actualForce.z =
                 contacts.contact(i).wrench(j).body_1_wrench().force().z();
-                
-            std::cout << j << "\n  FORCE:" << actualForce << std::endl;
-            std::cout << j << "\n  FORCE magnitude:" << actualForce.GetLength() << std::endl;       
-            
-            
-            msg.data = actualForce.GetLength();
-            this->rosPub.publish(msg);                 
+            // std::cout << j << "\n  FORCE:" << actualForce << std::endl;
+            // std::cout << j << "\n  FORCE magnitude:" << actualForce.GetLength() << std::endl;       
         }
     }
     
-    /*for (unsigned int j = 0; j < contacts.contact(i).position_size(); ++j) {
-      std::cout << j << "  Position:"
-                << contacts.contact(i).position(j).x() << " "
-                << contacts.contact(i).position(j).y() << " "
-                << contacts.contact(i).position(j).z() << "\n";
-      std::cout << "   Normal:"
-                << contacts.contact(i).normal(j).x() << " "
-                << contacts.contact(i).normal(j).y() << " "
-                << contacts.contact(i).normal(j).z() << "\n";
-      std::cout << "   Depth:" << contacts.contact(i).depth(j) << "\n";
-    }*/
-    
+    msg.data = actualForce.GetLength();
+    this->rosPub.publish(msg);                 
     
   }
 }
